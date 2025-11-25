1584c00d06c627d7c283b8ed475cfc0f507dc42c
bagoesrex <bagoesrex@gmail.com>
2025-11-25 22:41:22 +0700

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <h1 className="font-bold text-2xl animate-bounce underline">Membaca Api :D</h1>
+    </main>
+  );
+}
