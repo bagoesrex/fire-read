15f33dd37dda06309f4de6aa110600fc7c608a9b
bagoesrex <bagoesrex@gmail.com>
2025-11-27 23:29:02 +0700

    feat(layout): add initial header component

---
+++ components/layout/header.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Book, Flame } from "lucide-react"
+import Link from "next/link"
+import { Button } from "../ui/button"
+
+export default function Header() {
+    return (
+        <header className="border-b border-b-gray-400 top-0 z-50">
+            <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3">
+                <div className="flex items-center gap-2">
+                    <div className="relative">
+                        <Book size={30} strokeWidth={1} strokeOpacity={0.6} className="-rotate-4" />
+                        <Flame size={15} className="absolute top-0 -right-1 animate-bounce" color="orange" strokeWidth={2} />
+                        <Flame size={10} className="absolute -top-1.5 right-1.5 animate-bounce" color="orange" strokeWidth={2} />
+                        <Flame size={7} className="absolute -top-2 -right-1 animate-bounce" color="orange" strokeWidth={3} />
+                    </div>
+                    <p className="text-2xl font-semibold">Firebook</p>
+                </div>
+
+                <nav className="space-x-4">
+                    <Link href="/">Books</Link>
+                    <Link href="/">Categories</Link>
+                    <Link href="/">Questions</Link>
+                    <Link href="/">About</Link>
+                </nav>
+
+                <div className="flex items-center space-x-2">
+                    <Button variant={"ghost"}>Sign in</Button>
+                    <Button>
+                        <Flame />
+                        Get Started
+                    </Button>
+                </div>
+            </div>
+        </header>
+    )
+}
